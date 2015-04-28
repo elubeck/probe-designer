dd1a0dfccf3c6e43f99f1723d6891df0356761cf
Eric Lubeck <elubeck@caltech.edu>
2015-04-28 16:20:52 -0700

    Modified oligoarray to kill zombie blast process and error out after timeout.

--- probe_designer/oligoarray_designer.py
+++ probe_designer/oligoarray_designer.py
@@ -7,6 +7,24 @@ import csv
 from itertools import groupby
 import pandas as pd
 from random import getrandbits
+import signal
+from time import sleep
+
+class TimeoutError(Exception):
+    pass
+
+
+class timeout(object):
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
 
 class Oligoarray(object):
 
