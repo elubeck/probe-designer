d79e9a2f13ac1c7f83dfea24318f54a3c791dc8f
Eric <elubeck@caltech.edu>
2016-02-11 13:49:09 -0800

    Misc Bugs

--- probe_designer/chip_designs/example_design.py
+++ probe_designer/chip_designs/example_design.py
@@ -6,6 +6,7 @@ from probe_designer.mRNA_designer import RNARetriever2
 retriever = RNARetriever2()
 
 pgk1 = retriever.get_single_rna("Pgk1", cds_only=True)
+pgk1 = list(pgk1)
 print("BOB")
 print(len(pgk1),)
 
