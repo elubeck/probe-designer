c566d2aee519a37551d8481fa50a93fe7bd1b83e
Eric <elubeck@caltech.edu>
2016-02-11 14:04:03 -0800

    Tweaks

--- probe_designer/chip_designs/example_design.py
+++ probe_designer/chip_designs/example_design.py
@@ -8,6 +8,7 @@ retriever = RNARetriever2()
 filterer = ProbeFilter(db='gencode_tracks_reversed', copy_num='brain')
 
 name, probes, sequence = design_step('Pgk1', cds_only=True, length=35)
+probes = set(probes)
 final_probes = filterer.run(probes, name, match_thresh=18, n_probes=48, max_off_target=2000)
 
 
