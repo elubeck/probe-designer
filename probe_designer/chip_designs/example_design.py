31706976f0ed99305c4f141a4df9f84c87cf9772
Eric <elubeck@caltech.edu>
2016-02-11 13:55:34 -0800

    Tweak design

--- probe_designer/chip_designs/example_design.py
+++ probe_designer/chip_designs/example_design.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
-from probe_designer.mRNA_designer import RNARetriever2
+from probe_designer.mRNA_designer import RNARetriever2, design_step
 
 retriever = RNARetriever2()
 
 pgk1 = retriever.get_single_rna("Pgk1", cds_only=True)
 pgk1 = list(pgk1)
+
+pgk1_probes = design_step('Pgk1', cds_only=True, length=35)
 print("BOB")
 print(len(pgk1),)
 
