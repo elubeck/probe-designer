52a2174f236cf972d3cc210429c8d4771e54b645
Eric <elubeck@caltech.edu>
2016-02-11 14:10:03 -0800

    More params

--- probe_designer/chip_designs/example_design.py
+++ probe_designer/chip_designs/example_design.py
@@ -5,7 +5,7 @@ from probe_designer.mRNA_designer import RNARetriever2, design_step
 from probe_designer.probe_refiner import ProbeFilter
 
 retriever = RNARetriever2()
-filterer = ProbeFilter(db='gencode_tracks_reversed', copy_num='brain')
+filterer = ProbeFilter(db='gencode_tracks_reversed+mRNA', copy_num='brain')
 
 name, probes, sequence = design_step('Pgk1', cds_only=True, length=35,
                                      spacing=1, gc_target=0.55, gc_min=None, gc_max=None)
@@ -14,4 +14,3 @@ final_probes = filterer.run(probes, name, match_thresh=18, n_probes=48, max_off_
 
 print(len(final_probes))
 
-print("BOB")
