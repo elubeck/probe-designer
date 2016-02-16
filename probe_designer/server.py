a506fb23a00114081d2a56af8c7364c820601a2b
Eric <elubeck@caltech.edu>
2016-02-16 12:08:21 -0800

    meh

--- probe_designer/server.py
+++ probe_designer/server.py
@@ -31,7 +31,7 @@ def index():
         gc_target = form.gc_target.data
     else:
         genes=None
-    return render_template("view.html", form=form, s=genes)
+    return render_template("view.html", form=form, )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
