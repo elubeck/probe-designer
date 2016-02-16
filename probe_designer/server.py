ab309b8a7b2e761eb84606300a4b21d64f5b13ab
Eric <elubeck@caltech.edu>
2016-02-16 08:24:23 -0800

    meh

--- probe_designer/server.py
+++ probe_designer/server.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request
-from wtforms import Form, TextField, validators
+from wtforms import Form, TextField, validators, SelectField
 
 app = Flask(__name__)
 
 # Model
 class InputForm(Form):
-    r = TextField(validators=[validators.InputRequired()])
+    genes = TextField(validators=[validators.InputRequired()])
+    select = SelectField(u'Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
 
 # View
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = InputForm(request.form)
     if request.method == 'POST' and form.validate():
-        r = form.r.data
+        r = form.genes.data
+        selection = form.select.data
         return render_template("view_output.html", form=form, s=r)
     else:
         return render_template("view_input.html", form=form)
