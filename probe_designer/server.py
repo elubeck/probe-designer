625edab67374c30d1382bca975fbbe360ea58d2e
Eric <elubeck@caltech.edu>
2016-02-16 11:58:04 -0800

    meh

--- probe_designer/server.py
+++ probe_designer/server.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template, request
-from wtforms import Form, TextField, validators, SelectField, DecimalField, BooleanField
+from wtforms import Form, TextField, validators, SelectField, DecimalField, BooleanField, IntegerField
 
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+Bootstrap(app)
 
 # Model
 class InputForm(Form):
-    genes = TextField(validators=[validators.InputRequired()])
-    gc_target = DecimalField(default=0.55, validators=[validators.InputRequired()])
+    std_validation = [validators.InputRequired()]
+    non_neg_val = [validators.InputRequired(), validators.NumberRange(min=0)]
+    genes = TextField(validators=std_validation)
+    gc_target = DecimalField(default=0.55, validators=std_validation)
+    gc_min = DecimalField(default=0.35, validators=std_validation)
+    gc_max = DecimalField(default=0.75, validators=std_validation)
     cds_only = BooleanField(label='CDS Only', default=True)
+    probe_length= IntegerField(default=35, validators=[validators.InputRequired(),
+                                                       validators.NumberRange(min=14)])
+    spacing = IntegerField(default=1, validators=non_neg_val)
+    max_probes = IntegerField(default=48, validators=non_neg_val)
+    false_pos_len = IntegerField(default=18, validators=non_neg_val)
+    max_off_target = IntegerField(default=50, validators=non_neg_val)
+    off_hits = IntegerField(default=6, validators=non_neg_val)
 
 # View
 @app.route('/', methods=['GET', 'POST'])
