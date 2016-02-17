f11a792d9bd8302504b3a587b2a14ca2c9f50d3d
Eric <elubeck@caltech.edu>
2016-02-16 16:36:23 -0800

    meh

--- probe_designer/server.py
+++ probe_designer/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask.ext.wtf import Form
-from wtforms import TextField, validators, SelectField, DecimalField, BooleanField, IntegerField, SubmitField, FloatField
+from wtforms import TextField, validators, SelectField, DecimalField, \
+    BooleanField, IntegerField, SubmitField, FloatField
 from probe_designer.mRNA_designer import RNARetriever2, design_step_gui
 from probe_designer.probe_refiner import ProbeFilter
 
@@ -31,7 +32,7 @@ class InputForm(Form):
     false_pos_len = IntegerField(default=18, validators=non_neg_val)
     max_off_target = IntegerField(default=50, validators=non_neg_val)
     off_hits = IntegerField(default=6, validators=non_neg_val)
-    copy_num = SelectField(default='brain',
+    copy_num_db = SelectField(default='brain',
                            validators=std_validation,
                            choices=[('brain', 'brain'), ('embryonic11.5', 'embryonic day 11.5')])
     submit_button = SubmitField('Submit Form')
@@ -40,19 +41,23 @@ def probes_2_str(probes, name):
     csv_str = ["{},{}".format(name, probe) for probe in probes]
     return "\n".join(csv_str)
 
+def parse_form(form):
+    csv = ""
+    for gene_name in form.data['genes'].split(','):
+        gene_name = gene_name.strip(' ')
+        name, probes1, seq = design_step_gui(gene_name, **form.data)
+        filterer = ProbeFilter(db='gencode_tracks_reversed_introns+mRNA',
+                               copy_num=form.data['copy_num_db'])
+        probes2 = filterer.run(set(probes1), name, **form.data)
+        csv = "\n".join([csv, probes_2_str(probes2, gene_name)])
+    return csv
+
 # View
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = InputForm(request.form)
     if request.method == 'POST' and form.validate():
-        csv = ""
-        for gene_name in form.data['genes'].split(','):
-            gene_name = gene_name.strip(' ')
-            name, probes1, seq = design_step_gui(gene_name, **form.data)
-            filterer = ProbeFilter(db='gencode_tracks_reversed_introns+mRNA', copy_num='brain')
-            probes2 = filterer.run(set(probes1), name, **form.data)
-            csv = "\n".join([csv, probes_2_str(probes2, gene_name)])
-        from flask import make_response
+        csv = parse_form(form)
         response = make_response(csv)
         response.headers["Content-Disposition"] = "attachment; filename=probes.csv"
         return response
