3b11ac786f36c15391c9b5e1bbc08efe0bd24171
Eric <elubeck@caltech.edu>
2016-02-23 22:09:00 -0800

    meh

--- probe_designer/server.py
+++ probe_designer/server.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request, make_response
 from flask.ext.basicauth import BasicAuth
 from flask.ext.wtf import Form
 from flask_bootstrap import Bootstrap
-from wtforms import validators, SelectField, BooleanField, IntegerField, SubmitField, FloatField, StringField
+from wtforms import validators, SelectField, BooleanField, IntegerField,\
+    SubmitField, FloatField, StringField, TextAreaField
 
 from probe_designer.mRNA_designer import design_step_gui
 from probe_designer.probe_refiner import ProbeFilter
@@ -26,21 +27,26 @@ Bootstrap(app)
 class InputForm(Form):
     std_validation = [validators.InputRequired()]
     non_neg_val = [validators.InputRequired(), validators.NumberRange(min=0)]
-    genes = StringField(validators=std_validation)
-    gc_target = FloatField(default=0.55, validators=std_validation)
-    gc_min = FloatField(default=0.35, validators=std_validation)
-    gc_max = FloatField(default=0.75, validators=std_validation)
-    cds_only = BooleanField(label='CDS Only', default=True)
-    length = IntegerField(default=35, validators=[validators.InputRequired(),
+    genes = TextAreaField(validators=std_validation)
+    gc_target = FloatField(label='Target GC', default=0.55, validators=std_validation)
+    gc_min = FloatField(label='Minimum GC', default=0.35, validators=std_validation)
+    gc_max = FloatField(label='Maximum GC', default=0.75, validators=std_validation)
+    length = IntegerField(label='Probe Length', default=35, validators=[validators.InputRequired(),
                                                   validators.NumberRange(min=14)])
-    spacing = IntegerField(default=1, validators=non_neg_val)
-    max_probes = IntegerField(default=48, validators=non_neg_val)
-    false_pos_len = IntegerField(default=18, validators=non_neg_val)
-    max_off_target = IntegerField(default=50, validators=non_neg_val)
-    off_hits = IntegerField(default=6, validators=non_neg_val)
-    copy_num_db = SelectField(default='brain',
+    spacing = IntegerField(label='Probe Spacing', default=1, validators=non_neg_val)
+    max_probes = IntegerField(label='Maximum Number of Probes', default=48, validators=non_neg_val)
+    false_pos_len = IntegerField(label='False Positive Match Length(nt)',
+                                 default=18, validators=non_neg_val)
+    max_off_target = IntegerField(label='Maximum Off-Target Threshold(AU)',
+                                  default=50, validators=non_neg_val)
+    off_hits = IntegerField(label='Maximum Off-Target Hits on Any Gene',
+                            default=6, validators=non_neg_val)
+    copy_num_db = SelectField(label='Copy Number Database',
+                              default='brain',
                               validators=std_validation,
                               choices=[('brain', 'brain'), ('embryonic11.5', 'embryonic day 11.5')])
+    cds_only = BooleanField(label='CDS Only', default=True)
+    output_name = StringField(label='Output CSV Name', default='probes.csv')
     submit_button = SubmitField('Submit Form')
 
 
@@ -48,18 +54,6 @@ def probes_2_str(probes, name):
     csv_str = ["{},{},{}".format(name, num, probe)
                for num, probe in enumerate(probes)]
     return "\n".join(csv_str)
-
-
-def parse_form(form):
-    csv = ""
-    for gene_name in form.data['genes'].split(','):
-        gene_name = gene_name.strip(' ')
-        name, probes1, seq = design_step_gui(gene_name, **form.data)
-        filterer = ProbeFilter(db='gencode_tracks_reversed_introns+mRNA',
-                               copy_num=form.data['copy_num_db'])
-        probes2 = filterer.run(set(probes1), name, **form.data)
-        csv = "\n".join([csv, probes_2_str(probes2, gene_name)])
-    return csv
 
 
 def parse_form2(form):
@@ -110,7 +104,7 @@ def index():
     if request.method == 'POST' and form.validate():
         csv = parse_form2(form)
         response = make_response(csv)
-        response.headers["Content-Disposition"] = "attachment; filename=probes.csv"
+        response.headers["Content-Disposition"] = "attachment; filename={}".format(form.data['output_name'])
         return response
     return render_template("view.html", form=form, )
 
